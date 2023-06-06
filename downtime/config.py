7969399c26cbded232a42f534e0cf0a1f3827e86
francesco <fspinna@gmail.com>
2023-06-06 17:20:58 +0200

    Add constants in config.py

--- downtime/config.py
+++ downtime/config.py
@@ -1,3 +1,16 @@
 import pathlib
 
 CACHE_FOLDER = pathlib.Path.home() / ".downtime_cache"
+
+GDRIVE_DATABASE_FILENAME = "database_gdrive.csv"
+GDRIVE_DATABASE_FILE_ID_COLUMN = "file_id"
+
+LOCAL_DATABASE_FILENAME = "database_local.csv"
+DATABASE_FOLDER_NAME_COLUMN = "folder_name"
+DATABASE_DATASET_NAME_COLUMN = "dataset_name"
+DATABASE_TASK_COLUMN = "task"
+DATABASE_FILENAME_COLUMN = "file"
+DATABASE_FILESIZE_COLUMN = "filesize"
+
+METADATA_LABELS_KEY = "labels"
+METADATA_TASK_KEY = "task"
